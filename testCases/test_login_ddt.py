22732e06e5f011a3dfc3afab132f559ebc29cd15
Awine Nickson <atwiinenicxon@gmail.com>
2020-11-18 16:45:57 +0300

    added the add browser option

--- testCases/test_login_ddt.py
+++ testCases/test_login_ddt.py
@@ -2,6 +2,7 @@ from pageObjects.LoginPage import LoginPage
 from utilities.readProperties import ReadConfig
 from utilities.customLogger import LogGen
 from utilities import XLUtils
+import time
 
 
 class Test_002_DDT_Login:
@@ -19,29 +20,34 @@ class Test_002_DDT_Login:
         self.rows = XLUtils.getRowCount(self.path, 'Sheet1')
         print("Number of rows in excel file:", self.rows)
 
-
+        lst_status = [] #empty list variable
         for r in range(2, self.rows+1):
             self.user = XLUtils.readData(self.path, 'Sheet1', r,1)
             self.password = XLUtils.readData(self.path, 'Sheet1', r, 2)
             self.exp = XLUtils.readData(self.path, 'Sheet1', r, 3)
 
+            self.lp.setUserName(self.user)
+            self.lp.setPassword(self.password)
+            self.lp.clickLogin()
+            time.sleep(5)
+            act_title = self.driver.title
+            exp_title = "Dashboard / nopCommerce administration"
 
-
-
-
-
-
-
-        self.lp.setUserName(self.username)
-        self.lp.setPassword(self.password)
-        self.lp.clickLogin()
-        act_title=self.driver.title
-        if act_title == "Dashboard / nopCommerce administration":
-            assert True
-            self.driver.close()
-            self.logger.info(" ********************* After login title passed *************** ")
-        else:
-            self.driver.save_screenshot(".//Screenshots//" + "test_login.png")
-            self.driver.close()
-            self.logger.error(" ********************************* After login title test failed ********************* ")
-            assert False
+            if act_title == exp_title:
+                if self.exp == "Pass":
+                    self.logger.info(" ****** Passed *********")
+                    self.lp.clickLogout()
+                    lst_status.append("Pass")
+                elif self.exp == "Fail":
+                    self.logger.info(" ****** Failed *******")
+                    self.lp.clickLogout()
+                    lst_status.append("Fail")
+            
+            elif act_title != exp_title:
+                if self.exp == 'Pass':
+                    self.logger.info(" ***** Failed ****** ")
+                    lst_status.append("Fail")
+                
+                elif self.exp == "Fail":
+                    self.logger.info("******* Passed *******")
+                    lst_status.append("Pass")
