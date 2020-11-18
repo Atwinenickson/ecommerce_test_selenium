9b5b0772d2aebb912e466a2db1807f81af0ad0c8
Awine Nickson <atwiinenicxon@gmail.com>
2020-11-18 14:14:58 +0300

    added the add browser option

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -2,6 +2,27 @@ from selenium import webdriver
 import pytest
 
 @pytest.fixture()
-def setup():
-    driver=webdriver.Chrome()
+def setup(browser):
+    if browser == 'chrome':
+        driver = webdriver.Chrome()
+        print("Launching Chrome browser...................")
+    elif browser == 'firefox':
+        driver = webdriver.Firefox()
+        print("Launching Firefox browser............")
+    elif browser == 'brave':
+        driver = webdriver.Chrome()
+        print("Launching Brave browser...................")
+    elif browser == 'ie':
+        driver = webdriver.Ie()
+        print("Launching IE browser...................")
+    elif browser == 'opera':
+        driver = webdriver.Opera()
+        print("Launching Opera browser...................")
     return driver
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+@pytest.fixture()
+def browser(request):
+    return request.config.getoption("--browser")
