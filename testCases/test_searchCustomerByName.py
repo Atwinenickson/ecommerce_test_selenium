32a09bbab306c90115ba0b072b782e20514b5252
Awine Nickson <atwiinenicxon@gmail.com>
2020-11-20 13:38:50 +0300

    added the search by email and customer

---
+++ testCases/test_searchCustomerByName.py
@@ -0,0 +1,44 @@
+from os import setgroups
+import time
+import pytest
+from pageObjects.LoginPage import LoginPage
+from pageObjects.AddcustomerPage import AddCustomer
+from pageObjects.SearchCustomerPage import SearchCustomer
+from utilities.readProperties import ReadConfig
+from utilities.customLogger import LogGen
+
+class Test_SearchCusomerByName_005:
+    baseURL = ReadConfig.getApplicationURL()
+    username = ReadConfig.getUseremail()
+    password = ReadConfig.getPassword()
+    logger = LogGen.loggen()
+
+    @pytest.mark.regression
+    def test_searchCustomerByName(self, setup):
+        self.logger.info("****************** SearchCustomerByNaME_0005****************")
+        self.driver=setup
+        self.driver.get(self.baseURL)
+        self.driver.maximize_window()
+
+        self.lp = LoginPage(self.driver)
+        self.lp.setUserName(self.username)
+        self.lp.setPassword(self.password)
+        self.lp.clickLogin()
+        self.logger.info("****************** Login Successful ***************")
+        self.logger.info("******************Starting Search Customer By Name*******************")
+
+        self.addcust = AddCustomer(self.driver)
+        self.addcust.clickOnCustomersMenu()
+        self.addcust.clickOnCustomersMenuItem()
+        time.sleep(5)
+
+        self.logger.info("************* Searching Customer by Name ****************")
+        searchcust = SearchCustomer(self.driver)
+        searchcust.setFirstName("Victoria")
+        searchcust.setLastName("Terces")
+        searchcust.clickSearch()
+        time.sleep(10)
+        status=searchcust.searchCustomerByName('Victoria Terces')
+        assert False == status
+        self.logger.info("************************* TC_SearchCustomerByName_005 finished **************")
+        self.driver.close()
