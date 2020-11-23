f8a80f3598db0eb978f929bd23c63573cf9599e3
Awine Nickson <atwiinenicxon@gmail.com>
2020-11-23 13:45:53 +0300

    added the venv for adding packages

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ python3 -m venv venv
 . venv/bin/activate
 pip install pytest
 pip install selenium
+pip install allure-pytest
+pip install openpyxl
+pip install pytest-xdist
+pip install pytest-html
 pytest -s -v -m "regression" testCases/ --browser chrome
