ca7e25ed8297d73fb695a986d9b27528688a4f17
Awine Nickson <atwiinenicxon@gmail.com>
2020-11-23 13:36:25 +0300

    added the venv for adding packages

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ python3 -m venv venv
 . venv/bin/activate
 pip install pytest
 pip install selenium
-pytest -s -v -m "regression" --html=./Reports/report.html testCases/ --browser chrome
+pytest -s -v -m "regression" testCases/ --browser chrome
