f23f2c3aeb69d73a7ceb383660f28a078737687f
Awine Nickson <atwiinenicxon@gmail.com>
2020-11-23 10:04:42 +0300

    group commands and make a bash and sh script to run them

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sleep 2 
-. venv/bin/activate
-pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
+# . venv/bin/activate
+pytest -s -v -m "regression" --html=./Reports/report.html testCases/ --browser chrome
